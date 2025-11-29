5eeb6d28ff6abd4140be36c37fc41dc8d7933152
Lau Mendoza <laumendoza@MacBook-Pro-de-Laura.local>
2025-11-29 15:50:05 -0300

    implementar map() para traer solo los nombres

--- proyectos/PLANETS-PROJECT/index.js
+++ proyectos/PLANETS-PROJECT/index.js
@@ -1,32 +1,37 @@
 
 
-const { parse } = require('csv-parse'); // Importa la librería 'csv-parse' para analizar archivos CSV
-const FileSystem = require('fs');       // Importa el módulo 'fs' para trabajar con el sistema de archivos
+const { parse } = require('csv-parse');
+const FileSystem = require('fs');
 
-const habitablePlanets = []; //array doe planetas habitables
+const habitablePlanets = [];
 
-function isHabitablePlanet(planet){
+//filtra planetas segun ciertas caracteristicas
+function isHabitablePlanet(planet) { 
     return planet['koi_disposition'] === 'CONFIRMED'
-    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
-    && planet['koi_prad'] < 1.6;
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
 }
-FileSystem.createReadStream("kepler_data.csv") // Crea un flujo de lectura para el archivo 'kepler_data.csv'
+
+// Crea un flujo de lectura para el archivo 'kepler_data.csv'
+FileSystem.createReadStream("kepler_data.csv") 
     .pipe(parse({
-        comment: '#', 
-        columns:true ,            // Configura el analizador para ignorar líneas que comienzan con '#'
+        comment: '#',
+        columns: true,
     }))
 
-.on('data', (data) => {           // Escucha el evento 'data' para procesar cada fila del archivo CSV 
-        if (isHabitablePlanet(data)){
+    .on('data', (data) => {
+        if (isHabitablePlanet(data)) {
             habitablePlanets.push(data);
-        }              // cada chunk of data lo añade al array results
+        }
     })
-    .on('error', (error) => {         // Escucha el evento 'error' para manejar errores durante la lectura del archivo
-        console.log(error);            // Imprime en consola el error si ocurre alguno
+    .on('error', (error) => {
+        console.log(error);
     })
-    .on('end', () => {      
+    .on('end', () => {
         console.log(`se encontraron ${habitablePlanets.length} planetas habitables! filtrados por tamaño, luminosidad que absorben del sol y tamaño`)          // Escucha el evento 'end' que se dispara cuando se ha leído todo el archivo
-       // console.log(habitablePlanets); 
-        console.log("done")          // Imprime en consola el array results con los datos leídos del archivo CSV
+        console.log(habitablePlanets.map((planet) => {
+            return planet['kepler_name'];
+        }))
+        console.log("done")
     });
 
