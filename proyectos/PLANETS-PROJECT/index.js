a9e917c7ab92fe1ad62884e0f6f669fb0ffdc6af
Lau Mendoza <laumendoza@MacBook-Pro-de-Laura.local>
2025-11-29 14:29:52 -0300

    convertir stream de bits y bytes en array de elemntos separados por coma: pipe(), parse()

--- proyectos/PLANETS-PROJECT/index.js
+++ proyectos/PLANETS-PROJECT/index.js
@@ -6,7 +6,12 @@ const FileSystem = require('fs');       // Importa el módulo 'fs' para trabajar
 const results = []; //array donde se almacenan los datos leídos del archivo CSV
 
 FileSystem.createReadStream("kepler_data.csv") // Crea un flujo de lectura para el archivo 'kepler_data.csv'
-    .on('data', (data) => {           // Escucha el evento 'data' para procesar cada fila del archivo CSV 
+    .pipe(parse({
+        comment: '#', 
+        columns:true ,            // Configura el analizador para ignorar líneas que comienzan con '#'
+    }))
+
+.on('data', (data) => {           // Escucha el evento 'data' para procesar cada fila del archivo CSV 
         results.push(data)              // cada chunk of data lo añade al array results
     })
     .on('error', (error) => {         // Escucha el evento 'error' para manejar errores durante la lectura del archivo
