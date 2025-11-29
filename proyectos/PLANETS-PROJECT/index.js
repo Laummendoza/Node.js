f68586087c5347429b148055ba0fca36850d5bc2
Lau Mendoza <laumendoza@MacBook-Pro-de-Laura.local>
2025-11-29 15:37:00 -0300

    implementar funcion para filtrar planetas habitables

--- proyectos/PLANETS-PROJECT/index.js
+++ proyectos/PLANETS-PROJECT/index.js
@@ -3,8 +3,13 @@
 const { parse } = require('csv-parse'); // Importa la librería 'csv-parse' para analizar archivos CSV
 const FileSystem = require('fs');       // Importa el módulo 'fs' para trabajar con el sistema de archivos
 
-const results = []; //array donde se almacenan los datos leídos del archivo CSV
+const habitablePlanets = []; //array doe planetas habitables
 
+function isHabitablePlanet(planet){
+    return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
 FileSystem.createReadStream("kepler_data.csv") // Crea un flujo de lectura para el archivo 'kepler_data.csv'
     .pipe(parse({
         comment: '#', 
@@ -12,13 +17,16 @@ FileSystem.createReadStream("kepler_data.csv") // Crea un flujo de lectura para 
     }))
 
 .on('data', (data) => {           // Escucha el evento 'data' para procesar cada fila del archivo CSV 
-        results.push(data)              // cada chunk of data lo añade al array results
+        if (isHabitablePlanet(data)){
+            habitablePlanets.push(data);
+        }              // cada chunk of data lo añade al array results
     })
     .on('error', (error) => {         // Escucha el evento 'error' para manejar errores durante la lectura del archivo
         console.log(error);            // Imprime en consola el error si ocurre alguno
     })
-    .on('end', () => {                // Escucha el evento 'end' que se dispara cuando se ha leído todo el archivo
-        console.log(results); 
+    .on('end', () => {      
+        console.log(`se encontraron ${habitablePlanets.length} planetas habitables! filtrados por tamaño, luminosidad que absorben del sol y tamaño`)          // Escucha el evento 'end' que se dispara cuando se ha leído todo el archivo
+       // console.log(habitablePlanets); 
         console.log("done")          // Imprime en consola el array results con los datos leídos del archivo CSV
     });
 
